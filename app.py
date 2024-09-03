a6fcea15e426cf2464f4253411b8a9a3fbfa62d1
ratiebareeng <bareengbiz@gmail.com>
2024-09-03 22:03:59 +0200

    Add scrape_artist route

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from crypt import methods
+
 from flask import Flask, request, jsonify
-from lyric_scraper import scrape_lyrics  # Ensure you have this module
+from lyric_scraper import scrape_lyrics, result  # Ensure you have this module
+from scraper import  request_song_url
 
 app = Flask(__name__)
 
@@ -13,6 +16,24 @@ def scrape():
 
     try:
         result = scrape_lyrics(url)  # Call your scraping function
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+# Scrape artist songs
+@app.route('/scrapeArtist', methods=['POST'])
+def scrape_artist():
+    data = request.get_json()
+    artist_name = data.get('artistName')
+    songCap = data.get('songCap')
+    if not artist_name:
+        return  jsonify({'error': 'Artist Name is required'}), 400
+    if not songCap:
+        songCap = 10
+
+    try:
+        result = request_song_url(artist_name, songCap)
         return jsonify(result)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
